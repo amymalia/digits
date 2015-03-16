fa52881d27133a7fc2fbe1b87275ae22535130eb
amymalia <amytakayesu@gmail.com>
2015-03-15 23:09:26 -1000

    added input validation
    Added validation to user input fields

--- app/views/formdata/ContactFormData.java
+++ app/views/formdata/ContactFormData.java
@@ -1,9 +1,15 @@
 package views.formdata;
 
+import java.util.List;
+import java.util.ArrayList;
+import play.data.validation.ValidationError;
+
 /**
  * Created by amytakayesu on 3/15/15.
  */
 public class ContactFormData {
+  private static final int NUM_TELEPHONE_DIGITS = 12;
+
   /**First name field*/
   public String firstName = "";
 
@@ -12,5 +18,27 @@ public class ContactFormData {
 
   /**Telephone field*/
   public String telephone = "";
+
+  public List<ValidationError> validate(){
+    List<ValidationError> errors = new ArrayList<>();
+
+    if (firstName == null || firstName.length() == 0) {
+      errors.add(new ValidationError("firstName" , "First name is required."));
+    }
+
+    if (lastName == null || lastName.length() == 0) {
+      errors.add(new ValidationError("lastName", "Last name is required."));
+    }
+
+    if (telephone == null || telephone.length() == 0) {
+      errors.add(new ValidationError("telephone", "Telephone is required."));
+    }
+
+    if (telephone.length() != NUM_TELEPHONE_DIGITS) {
+      errors.add(new ValidationError("telephone", "Telephone must be xxx-xxx-xxxx."));
+    }
+
+    return errors.isEmpty() ? null : errors;
+  }
 
 }
