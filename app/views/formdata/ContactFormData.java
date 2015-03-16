3ba75b5e74de8372e5f090066cd245ee94751bea
amymalia <amytakayesu@gmail.com>
2015-03-15 22:04:43 -1000

    added form functionality
    added form functionalities in controller, application, routes.

---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,16 @@
+package views.formdata;
+
+/**
+ * Created by amytakayesu on 3/15/15.
+ */
+public class ContactFormData {
+  /**First name field*/
+  public String firstName = "";
+
+  /**Last name field*/
+  public String lastName = "";
+
+  /**Telephone field*/
+  public String telephone = "";
+
+}
